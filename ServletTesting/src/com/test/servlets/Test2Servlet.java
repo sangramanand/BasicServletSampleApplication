b6938d4473304bc11e437c53a361f4a5810111b0
Digital <Digital@192.168.0.127>
2018-02-22 09:41:42 +0530

    basic servlet jsp tuts

---
+++ ServletTesting/src/com/test/servlets/Test2Servlet.java
@@ -0,0 +1,47 @@
+package com.test.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test2Servlet
+ */
+@WebServlet("/Test2Servlet")
+public class Test2Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test2Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String message = "This message is from Java Servlet!!!";
+		request.setAttribute("message", message);
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/response.jsp");
+		rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
